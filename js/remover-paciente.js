1be4351e27ac850d1a4ea3d08970b6af01a2e834
ValneiPereira <valnney@yahoo.com.br>
2020-06-03 17:04:25 -0300

    Remocao delegacao e Animacao

---
+++ js/remover-paciente.js
@@ -0,0 +1,15 @@
+var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick", function(event){
+    var alvoEvento= event.target;
+    var paiDoAlvo = alvoEvento.parentNode;
+    paiDoAlvo.remove();
+    //event.target.parentNode.remove();
+});
+
+/* pacientes.forEach(function (paciente) {
+    paciente.addEventListener("dblclick", function () {
+        console.log("Fui clicado");
+        this.remove();
+    });
+}); */
